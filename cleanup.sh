ea6426a201dee08aa81bc47b84f7a356fd42b8fb
snowflake024 <snowflake024@gmail.com>
2022-12-28 15:32:43 +0100

    cleanup

---
+++ cleanup.sh
@@ -0,0 +1,7 @@
+!#/bin/bash
+
+set -x 
+
+rm -rf /srv/docker_playground/vpn-site2site-gw/10_wireguard-server/vpn-server-conf/*
+rm -rf /srv/docker_playground/vpn-site2site-gw/20_wireguard-client1/vpn-client1-conf/*
+rm -rf /srv/docker_playground/vpn-site2site-gw/30_wireguard-client2/vpn-client2-conf/*
