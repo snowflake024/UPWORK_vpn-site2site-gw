92ecc3632c7772773ae6dc86f7910f8f8c7ee9b6
snowflake <59052038+snowflake024@users.noreply.github.com>
2023-01-05 10:28:38 +0200

    fix pathing

--- cleanup.sh
+++ cleanup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-set -x 
+set -x
 
-rm -rf /srv/docker_playground/vpn-site2site-gw/10_wireguard-server/vpn-server-conf/*
-rm -rf /srv/docker_playground/vpn-site2site-gw/10_wireguard-server/vpn-server-conf/.*
-rm -rf /srv/docker_playground/vpn-site2site-gw/20_wireguard-client1/vpn-client1-conf/*
-rm -rf /srv/docker_playground/vpn-site2site-gw/30_wireguard-client2/vpn-client2-conf/*
+rm -rf ./10_wireguard-server/vpn-server-conf/*
+rm -rf ./10_wireguard-server/vpn-server-conf/.*
+rm -rf ./20_wireguard-client1/vpn-client1-conf/*
+rm -rf ./30_wireguard-client2/vpn-client2-conf/*
